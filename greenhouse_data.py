2f67345c0727ebc2ff4773efdd9165ffe564f9a1
Igor Kindop <Igor.Kindop@gmail.com>
2026-06-20 22:59:03 +0200

    offset and limit functionality

--- greenhouse_data.py
+++ greenhouse_data.py
@@ -12,8 +12,8 @@ import numpy as np
 # simple SQL data table Model for Fertilizer
 class Fertilizer(SQLModel, table=True):
     id: int| None = Field(default=None, primary_key=True)
-    name: str | None = None
-    type: str | None = None
+    name: str | None = Field(index=True)
+    type: str | None = Field(default=None, index=True)
     NPK_ratio: str | None = None
     application_method: str | None = None
     application_frequency: str | None = None
@@ -30,62 +30,41 @@ def  create_fertilizers():
     fert1 = Fertilizer(name="Azofoska", type = "Liquid", NPK_ratio = "10-5.5-8", application_method = "Fertigation", application_frequency = "Twice a month")
     fert2 = Fertilizer(name="PermaBloom", type = "Liquid", NPK_ratio = "1-2-5", application_method = "Fertigation", application_frequency = "Twice a month")
     fert3 = Fertilizer(name="GreenGro", type = "Granular", NPK_ratio = "5-10-5", application_method = "Top dressing", application_frequency = "Once a month")
-    
-    print("Before interacting with DB")
-    print(fert1)
-    print(fert2)
-    print(fert3) 
-    
+    fert4 = Fertilizer(name="SuperGrow", type = "Granular", NPK_ratio = "20-10-10", application_method = "Top dressing", application_frequency = "Once a month")
+    fert5 = Fertilizer(name="OrganicManure", type = "Solid", NPK_ratio = "2-3-2", application_method = "Broadcasting", application_frequency = "Once a month")
+    fert6 = Fertilizer(name="HydroponicNutrient", type = "Liquid", NPK_ratio = "20-10-20", application_method = "Drip Irrigation", application_frequency = "Daily")
+
     # session = Session(engine)
     
     with Session(engine) as session:
         session.add(fert1)
         session.add(fert2)
-        
-        print("After adding to the DB")
-        print("Fertilizer 1:",fert1)
-        print("Fertilizer 2:",fert2)
-        print("Fertilizer 3:",fert3) 
+        session.add(fert3)
+        session.add(fert4)
+        session.add(fert5)
+        session.add(fert6)
 
         session.commit()
         
-        print("After commiting to the DB, show IDs")
-        print("Fertilizer 1:",fert1.id)
-        print("Fertilizer 2:",fert2.id)
-        print("Fertilizer 3:",fert3.id)
-        
-        print("After commiting to the DB, show names")
-        print("Fertilizer 1:",fert1.name)
-        print("Fertilizer 2:",fert2.name)
-        print("Fertilizer 3:",fert3.name) 
-        
         session.refresh(fert1)
         session.refresh(fert2)
-        # session.refresh(fert3)
+        session.refresh(fert3)
+        session.refresh(fert4)
+        session.refresh(fert5)
+        session.refresh(fert6)
 
-        print("After refreshing the fertilizers")
-        print("Fertilizer 1:", fert1)
-        print("Fertilizer 2:", fert2)
-        # print("Fertilizer 3:", fert3)
-    print("After the session closes")
-    print("Fertilizer 1:", fert1)
-    print("Fertilizer 2:", fert2)
-    print("Fertilizer 3:", fert3)
-    # session.add(fert1) # git add fert1
-    # session.add(fert2) # git add fert2
-    # session.commit() # git commit
-    # session.close() # git push
 
 def select_fertilizers():
     with Session(engine) as session:
-         statement = select(Fertilizer).where(Fertilizer.name!='PermaBloom').where(Fertilizer.type=='Liquid')
-         fertilizers = session.exec(statement) #SELECT * from fertilizers # and execute it
-         for object in fertilizers:
-             print(object)
+        #  statement = select(Fertilizer).where(Fertilizer.type=='Liquid')
+        statement = select(Fertilizer).where(Fertilizer.type!='Liquid').offset(2).limit(2)
+        fertilizers = session.exec(statement) #SELECT * from fertilizers # and execute it
+        object = fertilizers.all()
+        print(object)
 
 def main():
     create_db_and_tables()
-    create_fertilizers()
+    # create_fertilizers()
     select_fertilizers()
 
 if __name__ == "__main__":
